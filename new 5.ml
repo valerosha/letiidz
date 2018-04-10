b0f8a28901aecba8786b53f373e3185ecfafd468
valerosha <valeria__1995@mail.ru>
2018-04-10 19:04:34 +0300

    Update new 5.ml

--- new 5.ml
+++ new 5.ml
@@ -1,4 +1,4 @@
-pen GT       
+open GT       
 open Language
        
 (* The type for the stack machine instructions *)
@@ -18,19 +18,19 @@ open Language
                                                    
 (* The type for the stack machine program *)                                                               
 type prg = insn list
-                            
+
 (* The type for the stack machine configuration: control stack, stack and configuration from statement
    interpreter
  *)
-type config = (prg * State.t) list * int list * Expr.config
+type config = (prg * State.t) list * int list * Stmt.config
 
 (* Stack machine interpreter
      val eval : env -> config -> prg -> config
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
-*)                                                  
-let rec eval env ((cstack, stack, ((st, i, o, r) as c)) as conf) prg =
-	match prg with
+*)                         
+let rec eval env ((cstack, stack, ((st, i, o) as c)) as conf) prog =
+	match prog with
 	| [] -> conf
 	| inst :: tail -> 
 	  let cfg, next = 
@@ -40,10 +40,10 @@ let rec eval env ((cstack, stack, ((st, i, o, r) as c)) as conf) prg =
 					  y :: x :: st_end -> (cstack, (Expr.calc binop x y) :: st_end, c), tail
 					| _ -> failwith "Not enough arguments for binary operation")
 		  | CONST n -> (cstack, n :: stack, c), tail
-		  | READ -> let num = List.hd i in (cstack , num :: stack, (st, List.tl i, o, None)), tail
-		  | WRITE -> let num = List.hd stack in (cstack, List.tl stack, (st, i, o @ [num], None)), tail
+		  | READ -> let num = List.hd i in (cstack , num :: stack, (st, List.tl i, o)), tail
+		  | WRITE -> let num = List.hd stack in (cstack, List.tl stack, (st, i, o @ [num])), tail
 		  | LD x -> (cstack, (State.eval st x) :: stack, c), tail
-		  | ST x -> let num = List.hd stack in (cstack, List.tl stack, (State.update x num st, i, o, None)), tail
+		  | ST x -> let num = List.hd stack in (cstack, List.tl stack, (State.update x num st, i, o)), tail
 		  | LABEL _ -> conf, tail
 		  | JMP l -> conf, (env#labeled l)
 		  | CJMP (op, l) ->
@@ -53,13 +53,13 @@ let rec eval env ((cstack, stack, ((st, i, o, r) as c)) as conf) prg =
 		  | BEGIN (params, locals) ->
 				let new_st = State.enter st (params @ locals) in
 				let st', stack' = List.fold_left (fun (a_st, v :: a_stack) p -> State.update p v a_st, a_stack) (new_st, stack) params in
-				(cstack, stack', (st', i, o, None)), tail
+				(cstack, stack', (st', i, o)), tail
 		  | END -> 
 			   (match cstack with
 				| [] -> conf, []
 				| (prg', st') :: rest -> 
 					let new_st = State.leave st st' in
-					(rest, stack, (new_st, i, o, None)), prg')
+					(rest, stack, (new_st, i, o)), prg')
 		  | CALL proc -> ((tail, st) :: cstack, stack, c), (env#labeled proc)) in
 	  eval env cfg next
 
@@ -68,7 +68,6 @@ let rec eval env ((cstack, stack, ((st, i, o, r) as c)) as conf) prg =
    Takes a program, an input stream, and returns an output stream this program calculates
 *)
 let run p i =
-  (*print_prg p;*)
   let module M = Map.Make (String) in
   let rec make_map m = function
   | []              -> m
@@ -76,7 +75,7 @@ let run p i =
   | _ :: tl         -> make_map m tl
   in
   let m = make_map M.empty p in
-  let (_, _, (_, _, o, _)) = eval (object method labeled l = M.find l m end) ([], [], (State.empty, i, [], None)) p in o
+  let (_, _, (_, _, o)) = eval (object method labeled l = M.find l m end) ([], [], ({ g = State.empty; l = State.empty; scope = [] }, i, [])) p in o
 
 (* Stack machine compiler
      val compile : Language.t -> prg
@@ -87,22 +86,19 @@ let compile (defs, p) =
 	let label_creator =
 		object
 			val cur_val = 0
-			method name x = "proc_" ^ x
+			method name x = "label_" ^ x
 			method get = "label_" ^ string_of_int cur_val, {< cur_val = cur_val + 1 >}
 		end in
-	let rec compile_expr lbl (expr : Language.Expr.t) = 
-		(match expr with
+	let rec compile_expr (expr : Language.Expr.t) = 
+		match expr with
 		| Expr.Const n -> [CONST n]
 		| Expr.Var x -> [LD x]
-		| Expr.Binop (binop, x, y) -> compile_expr lbl x @ compile_expr lbl y @ [BINOP binop]
-		| Expr.Call (proc, params) -> 
-			let compiled_params = List.fold_right (fun expr lst -> lst @ (compile_expr lbl expr)) params [] in
-			(compiled_params @ [CALL (lbl#name proc)])) in
+		| Expr.Binop (binop, x, y) -> compile_expr x @ compile_expr y @ [BINOP binop] in
 	let rec compile_label lbl stmt = 
 	   (match stmt with
 		  Stmt.Read x -> lbl, [READ; ST x]
-		| Stmt.Write expr -> lbl, (compile_expr lbl expr) @ [WRITE]
-		| Stmt.Assign (x, expr) -> lbl, (compile_expr lbl expr) @ [ST x]
+		| Stmt.Write expr -> lbl, (compile_expr expr) @ [WRITE]
+		| Stmt.Assign (x, expr) -> lbl, (compile_expr expr) @ [ST x]
 		| Stmt.Seq (stmt_left, stmt_right) ->
 			let lbl, left = compile_label lbl stmt_left in
 			let lbl, right = compile_label lbl stmt_right in
@@ -117,23 +113,19 @@ let compile (defs, p) =
 				match f with
 				| Skip -> [LABEL flbl]
 				| _ -> [JMP endlbl; LABEL flbl] @ iff @ [LABEL endlbl] in
-			lbl, (compile_expr lbl cond) @ [CJMP ("z", flbl)] @ ift @ instr
+			lbl, (compile_expr cond) @ [CJMP ("z", flbl)] @ ift @ instr
 		| Stmt.While (cond, st) ->
 			let initlbl, lbl = lbl#get in
 			let endlbl, lbl = lbl#get in
 			let lbl, body = compile_label lbl st in
-			lbl, [JMP endlbl; LABEL initlbl] @ body @ [LABEL endlbl] @ (compile_expr lbl cond) @ [CJMP ("nz", initlbl)]
+			lbl, [JMP endlbl; LABEL initlbl] @ body @ [LABEL endlbl] @ (compile_expr cond) @ [CJMP ("nz", initlbl)]
 		| Stmt.Repeat (st, cond) ->
 			let initlbl, lbl = lbl#get in
 			let lbl, body = compile_label lbl st in
-			lbl, [LABEL initlbl] @ body @ (compile_expr lbl cond) @ [CJMP ("z", initlbl)]
-		| Stmt.Return expr -> 
-			let ret =
-			(match expr with
-			| None -> []
-			| Some e -> compile_expr lbl e) in
-			lbl, ret @ [END]
-		| Stmt.Call (proc, params) -> lbl, compile_expr lbl (Expr.Call (proc, params))) in
+			lbl, [LABEL initlbl] @ body @ (compile_expr cond) @ [CJMP ("z", initlbl)]
+		| Stmt.Call (proc, params) ->
+			let compiled_params = List.fold_right (fun expr lst -> lst @ (compile_expr expr)) params [] in
+			lbl, (compiled_params @ [CALL (lbl#name proc)])) in
 	let rec compile_def lbl procs =
 		match procs with
 		| [] -> lbl, []
